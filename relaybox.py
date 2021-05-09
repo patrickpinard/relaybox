1129d95de03ab12c40b9820753bc15534c9abb74
Patrick Pinard <ppinard@bluewin.ch>
2021-05-09 11:00:56 +0200

    Update relaybox.py

--- relaybox.py
+++ relaybox.py
@@ -161,4 +161,4 @@ if __name__ == "__main__":
     logging.info("program starting...")
     logging.info("pins definition : " + str(pins))
     
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
